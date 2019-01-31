587f915fc9e2859cd324e86590a0d62a1e98b131
Shaance <christophe.ha1@gmail.com>
2019-02-01 00:08:41 +0100

    first commit

---
+++ src/main/java/com/shaance/catmashinterview/CatmashInterviewApplication.java
@@ -0,0 +1,14 @@
+package com.shaance.catmashinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatmashInterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatmashInterviewApplication.class, args);
+	}
+
+}
+
