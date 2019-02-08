bfbe95c0ccbe04dbe04da96bb3582f2c776af98b
Shaance <christophe.ha1@gmail.com>
2019-02-08 22:17:34 +0100

    Refactor test to use mock instead of real RestTemplate call

--- src/test/java/com/shaance/catmashinterview/service/CatConnectorImplTest.java
+++ src/test/java/com/shaance/catmashinterview/service/CatConnectorImplTest.java
@@ -1,12 +1,22 @@
 package com.shaance.catmashinterview.service;
 
 import com.shaance.catmashinterview.connector.CatConnectorImpl;
+import com.shaance.catmashinterview.dto.CatDataDto;
+import com.shaance.catmashinterview.dto.CatDto;
 import com.shaance.catmashinterview.entity.Cat;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.assertEquals;
@@ -14,11 +24,15 @@ import static org.junit.Assert.assertTrue;
 
 public class CatConnectorImplTest {
 
+	@Mock
+	private RestTemplate restTemplate;
+
+	@InjectMocks
 	private CatConnectorImpl catConnector;
 
 	@Before
 	public void setUp() {
-		catConnector = new CatConnectorImpl();
+		MockitoAnnotations.initMocks(this);
 	}
 
 	@Test(expected = NullPointerException.class)
@@ -26,20 +40,26 @@ public class CatConnectorImplTest {
 		catConnector.getCatsFromURI(new URI(null));
 	}
 
-	/*
-	* URL could change leading to test failing.
-	* Should extract the part which retrieve ResponseEntity<CatDataDto> into another component
-	* and then mock it
-	* */
 	@Test
 	public void getCatsFromURI() throws URISyntaxException {
-		Stream<Cat> result = catConnector.getCatsFromURI(new URI("http://latelier.co/data/cats.json"));
+		URI validUri = new URI("http://latelier.co/data/cats.json");
+		CatDataDto catDataDto = new CatDataDto();
+		catDataDto.setImages(Arrays.asList(
+				new CatDto("id1", "url1"),
+				new CatDto("id2", "url2"))
+		);
+		Mockito.when(restTemplate.getForEntity(validUri, CatDataDto.class))
+				.thenReturn(ResponseEntity.ok(catDataDto));
+		Stream<Cat> result = catConnector.getCatsFromURI(validUri);
 		assertTrue(result.count() > 0);
 	}
 
 	@Test
 	public void getCatsFromBadURI() throws URISyntaxException {
-		Stream<Cat> result = catConnector.getCatsFromURI(new URI("http://latelier.co/data/catsxx.json"));
+		URI invalidUri = new URI("http://latelier.co/data/cats.json");
+		Mockito.when(restTemplate.getForEntity(invalidUri, CatDataDto.class))
+				.thenThrow(HttpClientErrorException.class);
+		Stream<Cat> result = catConnector.getCatsFromURI(invalidUri);
 		assertEquals(0, result.count());
 	}
 
