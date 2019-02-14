07a883d7da8f67b1ba2fec891fa54f6582ea917d
Shaance <christophe.ha1@gmail.com>
2019-02-14 21:59:58 +0100

    Improve readability of getCatWithWinningRatioFluxWithPredicate function

--- src/main/java/com/shaance/catmashinterview/service/CatMashStatisticServiceImpl.java
+++ src/main/java/com/shaance/catmashinterview/service/CatMashStatisticServiceImpl.java
@@ -5,6 +5,7 @@ import com.shaance.catmashinterview.dao.CatMashRecordDao;
 import com.shaance.catmashinterview.dto.CatDto;
 import com.shaance.catmashinterview.dto.CatWithNumberOfVotesDto;
 import com.shaance.catmashinterview.dto.CatWithWinningRatioDto;
+import com.shaance.catmashinterview.entity.Cat;
 import com.shaance.catmashinterview.entity.CatMashRecord;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +17,7 @@ import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -75,38 +77,48 @@ public class CatMashStatisticServiceImpl implements CatMashStatisticService {
 
 		return catMashRecordDao.findAll()
 				.filter(filterCondition)
-				.doOnNext(catMashRecord -> {
-					String winnerCatId = catMashRecord.getWinnerCatId();
-					String loserCatId = catMashRecord.getLoserCatId();
-
-					if (catStatsMap.containsKey(winnerCatId)) {
-						Pair<Float, Float> pair = catStatsMap.get(winnerCatId);
-						catStatsMap.put(winnerCatId, Pair.of(pair.getFirst() + 1f, pair.getSecond()));
-					} else {
-						catStatsMap.put(winnerCatId, Pair.of(1f, 0f));
-					}
-
-					if (catStatsMap.containsKey(loserCatId)) {
-						Pair<Float, Float> pair = catStatsMap.get(loserCatId);
-						catStatsMap.put(loserCatId, Pair.of(pair.getFirst(), pair.getSecond() + 1f));
-					} else {
-						catStatsMap.put(loserCatId, Pair.of(0f, 1f));
-					}
-
-
-				}).thenMany(catDao.findAll()
-						.map(cat -> {
-							if (catStatsMap.containsKey(cat.getId())) {
-								Pair<Float, Float> floatPair = catStatsMap.get(cat.getId());
-								float winningRatio = floatPair.getFirst() / (floatPair.getFirst() + floatPair.getSecond());
-								return new CatWithWinningRatioDto(new CatDto(cat.getId(), cat.getUrl().toString()), winningRatio);
-							} else {
-								return new CatWithWinningRatioDto(new CatDto(cat.getId(), cat.getUrl().toString()), -1f);
-							}
-						})
+				.doOnNext(catMashRecord -> fillCatStatsMap(catMashRecord, catStatsMap))
+				.thenMany(catDao.findAll()
+						.map(cat -> catToCatWithWinningRatioDto(cat, catStatsMap))
 						.sort(Comparator.comparing(CatWithWinningRatioDto::getWinningRatio).reversed())
 				);
+	}
 
+	private CatWithWinningRatioDto catToCatWithWinningRatioDto(Cat cat, Map<String, Pair<Float, Float>> catStatsMap){
+
+		if (catStatsMap.containsKey(cat.getId())) {
+			Pair<Float, Float> floatPair = catStatsMap.get(cat.getId());
+			float winningRatio = floatPair.getFirst() / (floatPair.getFirst() + floatPair.getSecond());
+			return new CatWithWinningRatioDto(new CatDto(cat.getId(), cat.getUrl().toString()), winningRatio);
+		} else {
+			return new CatWithWinningRatioDto(new CatDto(cat.getId(), cat.getUrl().toString()), -1f);
+		}
 
 	}
+
+	private void fillCatStatsMap(CatMashRecord catMashRecord, Map<String, Pair<Float, Float>> catStatsMap){
+
+		Consumer<CatMashRecord> consumer = (c) -> {
+			String winnerCatId = c.getWinnerCatId();
+			String loserCatId = c.getLoserCatId();
+
+			if (catStatsMap.containsKey(winnerCatId)) {
+				Pair<Float, Float> pair = catStatsMap.get(winnerCatId);
+				catStatsMap.put(winnerCatId, Pair.of(pair.getFirst() + 1f, pair.getSecond()));
+			} else {
+				catStatsMap.put(winnerCatId, Pair.of(1f, 0f));
+			}
+
+			if (catStatsMap.containsKey(loserCatId)) {
+				Pair<Float, Float> pair = catStatsMap.get(loserCatId);
+				catStatsMap.put(loserCatId, Pair.of(pair.getFirst(), pair.getSecond() + 1f));
+			} else {
+				catStatsMap.put(loserCatId, Pair.of(0f, 1f));
+			}
+		};
+
+		consumer.accept(catMashRecord);
+
+	}
+
 }
